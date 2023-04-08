0fad7bbec7b77ddfec1eb60db90b50458dd38280
Skitaylov <111139772+Skitaylov@users.noreply.github.com>
2023-04-08 22:34:27 +0300

    Delete ExcursionGroup.java

--- app/src/main/java/net/unlucky/excursionticketapp/excursion/ExcursionGroup.java
+++
@@ -1,9 +0,0 @@
-package net.unlucky.excursionticketapp.excursion;
-
-import net.unlucky.excursionticketapp.ticket.Ticket;
-
-import java.util.ArrayList;
-
-public class ExcursionGroup {
-    ArrayList<Ticket> tickets = new ArrayList<>();
-}
